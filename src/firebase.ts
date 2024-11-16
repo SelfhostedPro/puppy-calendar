6ec619a7ab5c6468287d8b87c8201b2a828b9aef
SelfhostedPro <info@selfhosted.pro>
2024-11-15 21:38:01 -0800

    fixing firebase settings

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,22 +2,18 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-export function useDB() {
-  "use-server"
-  const firebaseConfig = {
-    apiKey: import.meta.env.FB_apiKey,
-    authDomain: import.meta.env.FB_authDomain,
-    projectId: import.meta.env.FB_projectId,
-    storageBucket: import.meta.env.FB_storageBucket,
-    messagingSenderId: import.meta.env.FB_messagingSenderId,
-    appId: import.meta.env.FB_appId,
-    measurementId: import.meta.env.FB_measurementId
-  };
-  return initializeApp(firebaseConfig);
-}
+const firebaseConfig = {
+  apiKey: import.meta.env.FB_apiKey,
+  authDomain: import.meta.env.FB_authDomain,
+  projectId: import.meta.env.FB_projectId,
+  storageBucket: import.meta.env.FB_storageBucket,
+  messagingSenderId: import.meta.env.FB_messagingSenderId,
+  appId: import.meta.env.FB_appId,
+  measurementId: import.meta.env.FB_measurementId
+};
 
 
 
-const app = useDB()
+const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
