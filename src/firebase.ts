aaeae93a715b787461d0b4c423491ce9146d41ea
SelfhostedPro <info@selfhosted.pro>
2024-11-15 21:16:34 -0800

    moving firebase to .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import process from 'node:process'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNmQX8hplxTw7qQOr6CXtJ8kUGsRklPHs",
-  authDomain: "puppy-tracker-bolt.firebaseapp.com",
-  projectId: "puppy-tracker-bolt",
-  storageBucket: "puppy-tracker-bolt.appspot.com",
-  messagingSenderId: "339076550103",
-  appId: "1:339076550103:web:8b6d6b8b6d6b8b6d6b8b6d"
+  apiKey: process.env.FB_apiKey,
+  authDomain: process.env.FB_authDomain,
+  projectId: process.env.FB_projectId,
+  storageBucket: process.env.FB_storageBucket,
+  messagingSenderId: process.env.FB_messagingSenderId,
+  appId: process.env.FB_appId,
+  measurementId: process.env.FB_measurementId
 };
 
 const app = initializeApp(firebaseConfig);
